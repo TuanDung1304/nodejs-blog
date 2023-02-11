abcc71b5d8c109b51a0c6b5e13628a99d2dade1f
Tuan Dung <tuandung13401@gmail.com>
2023-02-11 12:50:43 +0700

    chore(model): add mongoDB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,13 +1,22 @@
-class SiteController {
-  // GET /news
-  index(req, res) {
-    res.render("home");
-  }
+const Course = require('../models/Course');
 
-  //GET /search
-  search(req, res) {
-    res.render("search");
-  }
+class SiteController {
+    // GET /news
+    index(req, res) {
+        Course.find({}, (err, course) => {
+            if (!err) {
+                res.json(course);
+            } else {
+                res.status(400).json({ err: 'ERROR!' });
+            }
+        });
+        // res.render("home");
+    }
+
+    //GET /search
+    search(req, res) {
+        res.render('search');
+    }
 }
 
 module.exports = new SiteController();
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-const express = require("express");
-const path = require("path");
-const morgan = require("morgan");
-const { engine } = require("express-handlebars");
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-const route = require("./routes");
+const route = require('./routes');
+const db = require('./config/db');
 
-app.use(express.static(path.join(__dirname, "public")));
+// connect to DB
+db.connect();
+
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -15,13 +19,13 @@ app.use(express.json());
 // app.use(morgan("combined"));
 
 // template engine
-app.engine(".hbs", engine({ extname: ".hbs" }));
-app.set("view engine", ".hbs");
-app.set("views", path.join(__dirname, "views"));
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 // route init
 route(app);
 
 app.listen(port, () => {
-  console.log(`http:://localhost:${port}`);
+    console.log(`App listening at http:://localhost:${port}`);
 });
